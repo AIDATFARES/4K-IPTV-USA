43a4a133b9ba0b583adac6ce07d81b45503e7ac7
AIDATFARES <AIDATFARES@users.noreply.github.com>
2026-08-19 18:20:09 +0100

    Update domain to iptvus4k.site in metadata and links

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/'],
     },
-    sitemap: 'https://www.4kiptvusa.com/sitemap.xml',
+    sitemap: 'https://www.iptvus4k.site/sitemap.xml',
   };
 }
